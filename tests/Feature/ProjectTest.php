5b9fb32c8068db561e7d72d9cffa63180ddec395
VeroniqueRo <veronique.rouault@campus-numerique-in-the-alps.com>
2018-05-24 15:01:46 +0200

    TEST validant qu’un utilisateur connecté peut ajouter un projet + TEST validant qu’un utilisateur non connecté ne peut pas ajouter un projet

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -8,6 +8,7 @@ use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 Use Illuminate\Auth\Authentication;
+use Illuminate\Auth\AuthenticationException;
 
 class ProjectTest extends TestCase
 {
@@ -21,7 +22,6 @@ class ProjectTest extends TestCase
         // Etant donné que la vue est bien associée à sa route
         // Lorsque l'on saisit l'url /projects
         $response = $this->get('/projects');
-
         // On obtient une réponse Status 200 OK
         $response->assertStatus(200);
     }
@@ -34,7 +34,6 @@ class ProjectTest extends TestCase
         // Etant donné que la vue est bien associée à sa route
         // Lorsque l'on saisit l'url /projects
         $response = $this->get('/projects');
-
         // La balise h1 contenant la liste des projets s'affiche dans la page
         $response->assertSee('<h1>Listes des projets</h1>');
     }
@@ -43,10 +42,8 @@ class ProjectTest extends TestCase
     {
         // Etant donné la création d'une base de données des projets
         $project = Factory("App\Project")->create();
-
         // Lorsque l'on saisit l'url /project
         $response = $this->get('/projects');
-
         // On voit le nom du projet dans la page
         $response->assertSee($project->ProjectTitle);
     }
@@ -55,10 +52,8 @@ class ProjectTest extends TestCase
     {
         // Etant donné une vue détaillée de chaque projet
         $project = Factory(Project::class)->create();
-
         // Lorsque l'on saisit l'url /project/id du projet
         $response = $this->get('/project/'.$project->id);
-
         // Le nom du projet est bien dans la page
         $response->assertSee($project->ProjectTitle);
     }
@@ -67,19 +62,19 @@ class ProjectTest extends TestCase
     {
         // Etant donné que je crée un projet
         $project = Factory(Project::class)->create();
-
         // Lorsque l'on saisit l'url /project/id du projet
         $response = $this->get('/project/'.$project->id);
-
         // Le descriptif du projet est bien dans la page
         $response->assertSee($project->Descriptive);
     }
 
+    /**
+     * Test unitaire de la relation One to Many entre les Model User et Project
+     */
     public function testModelRelation()
     {
-        // Etant donné une relation One to Many entre les Model User et Project
+        // Etant un projet créé
         $project = Factory(Project::class)->create();
-
         // Lorsqu'on appelle l'user_id de la table Projects
         $actual = $project->user_id;
         // Alors  on obtient le même id existant dans la table Users
@@ -87,16 +82,15 @@ class ProjectTest extends TestCase
         $this->assertEquals($expected, $actual);
 
 //        dump($actual, $expected);
+//        dump($project->user->name);
     }
 
     public function testNomAuteurSurPageDetail ()
     {
         // Etant donné une vue détaillée de chaque projet
         $project = Factory(Project::class)->create();
-
         // Lorsque l'on saisit l'url /project/id du projet
         $response = $this->get('/project/'.$project->id);
-
         // Le nom de l'auteur du projet s'affiche bien dans la page
         $response->assertSee($project->user->name);
 
@@ -107,13 +101,10 @@ class ProjectTest extends TestCase
     {
         // Etant donné un utilisateur créé
         $user = factory(User::class)->create();
-
         // Que l'on authentifie en tant qu'utilisateur actuel
         $this->actingAs($user);
-
         // Lorsque l'on va sur l'url de création de projet
         $response = $this->get('/projectAjout');
-
         // Le nom de l'auteur du projet s'affiche bien dans la page
         $response->assertSee('Bonjour '.$user->name);
     }
@@ -128,6 +119,24 @@ class ProjectTest extends TestCase
         $this->actingAs($user)
             ->get('/projectAjout')
             ->assertSee('Bonjour '.$user->name);
+        // Lorsque l'on soumet un formulaire d'ajout de projet
+        $projet = [
+            'projecttitle' => 'Test Ajout de Projet',
+            'projectdescriptive'=>'Projet test',
+        ];
+
+        $this->post('/projects/liste', $projet);
+        // Quand on se rend sur la page des projets
+        $response = $this->get('/projects');
+        // Alors le projet apparait dans la liste des projets
+        $response->assertSee('Test Ajout de Projet');
+    }
+
+    public function testUtilisateurNonConnectePeutPasAjouterNouveauProjet()
+    {
+        // Etant donné un utilisateur qui va sur le site sans se connecter
+        // On attend une exception unauthentification
+        $this->expectException(AuthenticationException::class);
 
         // Lorsque l'on soumet un formulaire d'ajout de projet
         $projet = [
@@ -137,13 +146,17 @@ class ProjectTest extends TestCase
 
         $this->post('/projects/liste', $projet);
 
-        // Quand on se rend sur la page des projets
-        $response = $this->get('/projects');
-
-        // Alors le projet apparait dans la liste des projets
-        $response->assertSee('Test Ajout de Projet');
     }
 
+    public function testUtilisateurNonConnecteNePeutAjouterNouveauProjet()
+    {
+        // Etant donné un utilisateur qui va sur le site sans se connecter
+        // On attend une exception unauthentification
+        $this->expectException(AuthenticationException::class);
+        // Lorsque l'on va sur la page de création de projet
+        $this->get('/projectAjout');
+
+    }
 
 
 }
